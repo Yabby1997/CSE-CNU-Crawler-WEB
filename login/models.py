8680ccbafff32048f55734a7356fae27829cb814
yabby <yabby1997@gmail.com>
2020-04-26 00:38:04 +0900

    OTO 방식으로 Profile 데이터 생성

--- login/models.py
+++ login/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	#account_id = models.IntegerField()
+	#portal_id = models.CharField(max_length=30)
+	#portal_pw = models.CharField(max_length=30)
+	#last_login = models.DateTimeField()
+	
+	def __str__(self):
+		return self.user
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
