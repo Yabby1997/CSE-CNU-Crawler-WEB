074844bd02af51073774b0a36049310baa000a4b
yabby <yabby1997@gmail.com>
2020-04-26 00:45:47 +0900

    text대신 고유ID를 받도록 수정

--- notice_data/models.py
+++ notice_data/models.py
@@ -6,7 +6,7 @@ class NoticeData(models.Model):
 	type = models.TextField()
 	date = models.TextField()
 	title = models.TextField()
-	text = models.TextField()
+	number = models.TextField()
 
 	def __str__(self):
 		return self.title
