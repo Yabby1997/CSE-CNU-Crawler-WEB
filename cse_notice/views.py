26e2e77fa08a6607e0170e72c70aaf60a3dc1d70
yabby <yabby1997@gmail.com>
2020-04-27 16:25:38 +0900

    elearn페이지에서 refresh로 학습 데이터 fetch까지 구현

--- cse_notice/views.py
+++ cse_notice/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
 from notice_data.models import NoticeData
+from login.models import Profile
+import elearn_crawler as ec
 
 
 def notice(request):
@@ -10,4 +13,9 @@ def notice(request):
 
 
 def elearn(request):
+	if request.method == "POST":
+		profile = Profile.objects.get(user=request.user)
+		context = {'profile':profile}
+		ec.fetch_and_save(profile)
+		return render(request, 'notice/elearn.html', context)
 	return render(request, 'notice/elearn.html')
