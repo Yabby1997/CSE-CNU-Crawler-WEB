8b546ca2b07bfea5fddfe4eae29df786bc53a2ca
yabby <yabby1997@gmail.com>
2020-05-01 02:08:25 +0900

    오류 수정

--- elearn_crawler.py
+++ elearn_crawler.py
@@ -196,11 +196,11 @@ def fetch_elearn():
             for status in statistics:
                 if status.text.find('미제출') != -1:
                     report_statistics[1] += 1
-                    endDateTime = dues[i - 1].text[26:].replace('\t', '').replace('\n', '').replace('\r', '')
+                    endDateTime = dues[i].text[26:].replace('\t', '').replace('\n', '').replace('\r', '')
                     days, hours = convert_time(endDateTime, "%y/%m/%d %H:%M")
                     reports2do.append(course_name + names[i - 1].text.strip().replace('\n', '').replace('\t', '') + '   제출까지 '+ days + '일 ' + hours + '시간 남음')
                     i += 1
-                elif status.text.find('미제출') != -1:
+                elif status.text.find('제출') != -1:
                     report_statistics[0] += 1
                     i += 1
 
