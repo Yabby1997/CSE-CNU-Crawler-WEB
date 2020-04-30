c106d4c17a92871454eb53d52a8252241c3c38f2
yabby <yabby1997@gmail.com>
2020-04-30 17:53:31 +0900

    가입/로그인 폼 미작성시 경고창 뜨도록 수정

--- login/views.py
+++ login/views.py
@@ -6,19 +6,20 @@ from .models import Profile
 
 def signup(request):
 	if request.method == "POST":
-		if request.POST["password"] == request.POST["pw_confirm"]:
-			user = User.objects.create_user(
-				username=request.POST["username"],
-				password=request.POST["password"]
-				)
-			
-			profile = Profile(
-				user=user,
-				portal_id=request.POST["id_portal"],
-				portal_pw=request.POST["pw_portal"]
-				)
-			profile.save()
-			return redirect('login')
+		username = request.POST['username']
+		password = request.POST['password']
+		confirm = request.POST['pw_confirm']
+		id_portal = request.POST['id_portal']
+		pw_portal = request.POST['pw_portal']
+
+		if not (username and password and confirm and id_portal and pw_portal):
+			return render(request, 'login/signup.html', {'error' : 'username or password is incorrect'})
+		else:
+			if password == confirm:
+				user = User.objects.create_user(username=username, password=password)
+				Profile(user=user, portal_id=id_portal, portal_pw=pw_portal).save()
+				auth.login(request, user)
+				return redirect('elearn')
 	return render(request, 'login/signup.html')
 
 
@@ -26,12 +27,16 @@ def login(request):
 	if request.method == "POST":
 		username = request.POST['username']
 		password = request.POST['password']
-		user = auth.authenticate(request, username=username, password=password)
-		if user is not None:
-			auth.login(request, user)
-			return redirect('notice')
-		else:
+
+		if not (username and password):
 			return render(request, 'login/login.html', {'error' : 'username or password is incorrect'})
+		else:
+			user = auth.authenticate(request, username=username, password=password)
+			if user is not None:
+				auth.login(request, user)
+				return redirect('elearn')
+			else:
+				return render(request, 'login/login.html', {'error' : 'username or password is incorrect'})
 	else:
 		return render(request, 'login/login.html')
 
