c2282f2eff23fc71b9807cbde8f6716898e43d05
yabby <yabby1997@gmail.com>
2020-05-06 02:48:16 +0900

    portal id 학번 9자로 제한

--- login/models.py
+++ login/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
+from django.core.validators import RegexValidator
 
 
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	portal_id = models.CharField(max_length=50)
-	portal_pw = models.CharField(max_length=300)
-	last_update = models.DateTimeField(default=timezone.now)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    portal_id = models.CharField(max_length=9, validators=[RegexValidator('^[0-9]*$')])
+    portal_pw = models.CharField(max_length=300)
+    last_update = models.DateTimeField(default=timezone.now)
